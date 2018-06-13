15be29f7d8aee4dbf3af393e7c5e1b878b95d581
xkel <admin1@CbUser1s-iMac.local>
2018-06-13 12:43:50 -0400

    add server function to controller

--- controller.js
+++ controller.js
@@ -1,3 +1,4 @@
+const http = require('http');
 
 //Controller
 function Controller(id, secret, token) {
@@ -7,6 +8,20 @@ function Controller(id, secret, token) {
     this.token = token;
 };
 
+Controller.prototype.basicServer = function basicServer(port) {
+
+    const server = http.createServer((req, res) => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`id: ${this.clientID}, secret: ${this.clientSecret}, token: ${this.token}`);
+    });
+    
+    server.listen(port, (err) => {
+        if(err){
+            return console.log("error", err);
+        }
+    
+    })
+};
 
 module.exports = Controller;
 
