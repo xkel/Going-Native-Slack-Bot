47c5f2f1406f203ebb7c84f33d0e5333b04cc9f2
xkel <xavierk98@yahoo.com>
2018-06-20 10:47:05 -0400

    clean up botjs file

--- bot.js
+++ bot.js
@@ -1,14 +1,4 @@
-var bot_auth, bot_access_token, important;
-
 var HTTPS = require('https');
-
-//setup
-//bot_auth = process.env.BOT_AUTH;
-
-//Get request returns a string containing list of channels and relevant information about them
-                        //can be used to acquire channel id   
-
-
 
 //sends a http request of type specified in options parameters
 function sendReq(options ){
@@ -70,34 +60,10 @@ function getChannels(){
     };
 
     sendReq(options);
-
-
-    // var channelReq = HTTPS.request(options, (res) => {
-
-    //     console.log(`STATUS: ${res.statusCode}`);
-    //     res.on('data', (chunk) => {
-    //       console.log(`BODY: ${chunk}`);
-    //       data += chunk;
-
-    //     });
-    //     res.on('end', () => {
-    //         console.log('no more data lol');
-    //     });
-    // });
-
-    // channelReq.on('error', function(err) {
-    //     console.log('error posting message ' + JSON.stringify(err));
-    // });
-    // channelReq.on('timeout', function(err){
-    //     console.log('timeout posting message ' + JSON.stringify(err));
-    // });
-    // channelReq.end()
-
 }
 
 function botPostMessage(message){
     var options, body;
-
     options = {
         hostname: 'www.slack.com',
         path: '/api/chat.postMessage',
@@ -106,40 +72,17 @@ function botPostMessage(message){
             'Authorization': 'Bearer xoxb-379052561712-383098524721-3bQUrQUSaURzIFY8OKrTEmE1',
             'Content-Type': 'application/json',
         }
-  
     };
-
     body = {
         'channel': 'CB5R6F1K6',
         'text': message
     };
-
     sendReq(options, body);
-
-    // var botReq = HTTPS.request(options, (res) => {
-    //     console.log(`STATUS: ${res.statusCode}`);
-    //     res.on(`data`, (chunk) => {
-    //         console.log(`BODY: ${chunk}`);
-    //         data += chunk;
-    //     });
-    //     res.on('end', () => {
-    //         console.log("No more data!");
-    //     }); 
-
-    // });
-    // botReq.on('error', function(err) {
-    //     console.log('error posting message '  + JSON.stringify(err));
-    //   });
-    //   botReq.on('timeout', function(err) {
-    //     console.log('timeout posting message '  + JSON.stringify(err));
-    //   });
-    // botReq.end(JSON.stringify(body));
 }
 
 //Test API runs the first test API method to verify if things are running properly (for personal experience, not necessary at all)
 function testAPI(){
-    var options, data;
-    data = '';
+    var options;
     options = {
         hostname: 'www.slack.com',
         path: '/api/api.test',
@@ -149,32 +92,8 @@ function testAPI(){
             'Content-Type': 'application/json',
         }
     };
-
-    var testReq = HTTPS.request(options, (res) => {
-        console.log(`STATUS: ${res.statusCode}`);
-        res.on('data', (chunk) => {
-          //console.log(`BODY: ${chunk}`);
-          data += chunk;
-
-        });
-        res.on('end', () => {
-            data = JSON.parse(data);
-            //important = data.ok;
-        });
-    });
-
-    testReq.on('error', function(err) {
-        console.log('error posting message ' + JSON.stringify(err));
-    });
-    testReq.on('timeout', function(err){
-        console.log('timeout posting message ' + JSON.stringify(err));
-    });
-    testReq.end()
-
+    sendReq(options);
 }
 
-//getChannels();
 botPostMessage("Test1");
-//testAPI();
-//console.log(important);
 // exports.testAPI = testAPI;
