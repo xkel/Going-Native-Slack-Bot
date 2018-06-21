00bf5d6ef0b444e002ffc000543689d64932331a
xkel <xavierk98@yahoo.com>
2018-06-21 12:03:03 -0400

    dane is a genius

--- bot.js
+++ bot.js
@@ -1,50 +1,87 @@
 var HTTPS = require('https');
 
-//sends a http request of type specified in options parameters
-function sendReq(options ){
-    var data;
-    data = '';
 
-    var req = HTTPS.request(options, (res) => {
-        console.log(`STATUS: ${res.statusCode}`);
-        res.on('data', (chunk) => {
-            console.log(`BODY: ${chunk}`);
-            data += chunk;
+
+// function httpRequest(params, postData) {
+//     return new Promise(function(resolve, reject) {
+//         var req = http.request(params, function(res) {
+//             /
+//             // on response data, cumulate it
+//             // on end, parse and resolve
+//         });
+//         // on request error, reject
+//         // if there's post data, write it to the request
+//         // important: end the request req.end()
+//     });
+// }
+
+
+
+function sendReq(options, body ){
+ 
+    return new Promise(function(resolve, reject) {
+    
+        var req = HTTPS.request(options, (res) => {
+            if(res.statusCode < 200 || res.statusCode >= 300){
+                return reject(new Error('statusCode=' + res.statusCode));
+            }
+            var data = '';
+            console.log(`STATUS: ${res.statusCode}`);
+            res.on('data', (chunk) => {
+                console.log(`BODY: ${chunk}`);
+                data += chunk;
+            });
+            res.on('end', () => {
+                console.log("No more data!");
+                try{
+                    data = JSON.parse(data);
+
+                }catch(e){
+                    reject(e);
+                }
+                resolve(data);
+            });
         });
-        res.on('end', () => {
-            console.log("No more data!");
+
+        //reject on request error
+        req.on('error', function(err) {
+            console.log('error with request ' + JSON.stringify(err));
+            reject(error);
         });
+        req.on('timeout', function(err){
+            console.log('timeout with request ' + JSON.stringify(err));
+        });
+        if(body){
+            req.write(body);
+        }
+        
+        req.end();
+
     });
-    req.on('error', function(err) {
-        console.log('error with request ' + JSON.stringify(err));
-    });
-    req.on('timeout', function(err){
-        console.log('timeout with request ' + JSON.stringify(err));
-    });
-    req.end()
+
 }
 //sends a http request of type specified in options parameters, with an additional body argument
-function sendReq(options, body ){
-    var data;
-    data = '';
-    var req = HTTPS.request(options, (res) => {
-        console.log(`STATUS: ${res.statusCode}`);
-        res.on('data', (chunk) => {
-            console.log(`BODY: ${chunk}`);
-            data += chunk;
-        });
-        res.on('end', () => {
-            console.log("No more data!");
-        });
-    });
-    req.on('error', function(err) {
-        console.log('error with request ' + JSON.stringify(err));
-    });
-    req.on('timeout', function(err){
-        console.log('timeout with request ' + JSON.stringify(err));
-    });
-    req.end(JSON.stringify(body))
-}
+// function sendReq(options, body){
+//     var data;
+//     data = '';
+//     var req = HTTPS.request(options, (res) => {
+//         console.log(`STATUS: ${res.statusCode}`);
+//         res.on('data', (chunk) => {
+//             console.log(`BODY: ${chunk}`);
+//             data += chunk;
+//         });
+//         res.on('end', () => {
+//             console.log("No more data!");
+//         });
+//     });
+//     req.on('error', function(err) {
+//         console.log('error with request ' + JSON.stringify(err));
+//     });
+//     req.on('timeout', function(err){
+//         console.log('timeout with request ' + JSON.stringify(err));
+//     });
+//     req.end(JSON.stringify(body))
+// }
 
 function getChannels(){ 
     var options;
@@ -92,8 +129,13 @@ function testAPI(){
             'Content-Type': 'application/json',
         }
     };
-    sendReq(options);
+
+    sendReq(options).then(function(body) {
+        console.log(body.ok);
+    });
+    
 }
 
-botPostMessage("Test1");
-// exports.testAPI = testAPI;
+testAPI();
+
+
